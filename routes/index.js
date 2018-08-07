c20385c1d44d682e79c67bfdf113a8781b1d84d1
LeeSeongTack <bbyojjok@gmail.com>
2018-08-07 16:42:50 +0900

    modify

--- routes/index.js
+++ routes/index.js
@@ -5,24 +5,7 @@ const { getKSTDate } = require('../util');
 const ShortenUrl = require('../models/shortenUrl');
 const route = require('express').Router();
 
-route.get('/:urlCode?', async (req, res) => {
-	const urlCode = req.params.urlCode;
-	switch (urlCode) {
-		case undefined:
-			return res.sendFile(path.join(__dirname, '../', 'views/create.html'));
-		case 'error':
-			return res.sendFile(path.join(__dirname, '../', 'views/error.html'));
-		default:
-			const queryResult = await ShortenUrl.findOne({ urlCode: urlCode }, error => {
-				if (error) throw error;
-			});
-			if (queryResult) return res.redirect(301, queryResult.originalUrl);
-			else return res.redirect('/error');
-	}
-});
-
-route.post('/api/create', async (req, res) => {
-	const originalUrl = req.body.originalUrl;
+async function createShortenUrl(req, res, originalUrl) {
 	const shortBaseUrl = `${req.protocol}://${req.headers.host}/`;
 	const urlCode = shortid.generate();
 	const updatedAt = getKSTDate();
@@ -43,7 +26,7 @@ route.post('/api/create', async (req, res) => {
 				error => {
 				if (error) return res.status(401).send(`DB Error: ${error}`);
 			});
-			return res.status(200).json(updateResult);
+			return updateResult;
 		} else {
 			const shortUrl = shortBaseUrl + urlCode;
 			const queryResult = new ShortenUrl({
@@ -54,18 +37,78 @@ route.post('/api/create', async (req, res) => {
 			await queryResult.save((error) => {
 				if (error) return res.status(401).send(`DB Error: ${error}`);
 			});
-			return res.status(200).json(queryResult);
+			return queryResult;
 		}
 	} else {
-		return res.status(401).send('Error: 유효하지 않은 url');
+		return res.status(401).send('Error: Invaild url');
 	}
+}
+
+route.get('/:urlCode?', async (req, res) => {
+	const urlCode = req.params.urlCode;
+	switch (urlCode) {
+		case undefined:
+			return res.sendFile(path.join(__dirname, '../', 'views/create.html'));
+		case 'error':
+			return res.sendFile(path.join(__dirname, '../', 'views/error.html'));
+		default:
+			const queryResult = await ShortenUrl.findOne({ urlCode: urlCode }, error => {
+				if (error) throw error;
+			});
+			if (queryResult) return res.redirect(301, queryResult.originalUrl);
+			else return res.redirect('/error');
+	}
+});
+
+/**
+ * @param { string or object }
+ * { "originalUrl": "http://www.google.com" } // string
+ * { "originalUrl": ["http://naver.com", "http://daum.net", "http://www.google.com"] } // array
+ * @return { object }
+ */
+route.post('/api/create', async (req, res) => {
+	const originalUrl = req.body.originalUrl;
+	let result = [];
+	switch (typeof originalUrl) {
+		case 'string':
+			await result.push(createShortenUrl(req, res, originalUrl));
+			break;
+		case 'object':
+			for (var i=0; i < originalUrl.length; i++) {
+				await result.push(await createShortenUrl(req, res, originalUrl[i]));
+			}
+			break;
+		default:
+			return res.status(401).send('Error: Worng param');
+	}
+	console.log('# create');
+	return res.status(200).json(result);
 });
 
 route.post('/api/remove', async (req, res) => {
 	ShortenUrl.remove({}, error => {
 		if (error) return res.status(401).send(`DB Error: ${error}`);
+		console.log('# remove coll');
 		return res.status(200).send('collection removed');
 	});
+});
+
+route.get('/api/find/:urlCode?', async (req, res) => {
+	const urlCode = req.params.urlCode;
+	switch (urlCode) {
+		case undefined:
+			const findResult = await ShortenUrl.find({}, error => {
+				if (error) return res.status(401).send(`DB Error: ${error}`);
+			});
+			console.log('# find');
+			return res.status(200).json(findResult);
+		default:
+			const findOneResult = await ShortenUrl.findOne({ urlCode: urlCode }, error => {
+				if (error) return res.status(401).send(`DB Error: ${error}`);
+			});
+			console.log('# find');
+			return res.status(200).json(findOneResult);
+	}
 });
 
 module.exports = route;
