bc149fe928662b895fb914d398b5d2b8ac10a549
LeeSeongTack <bbyojjok@gmail.com>
2018-08-08 08:31:30 +0900

    modify

--- routes/index.js
+++ routes/index.js
@@ -10,17 +10,13 @@ async function createShortenUrl(req, res, originalUrl) {
 	const urlCode = shortid.generate();
 	const updatedAt = getKSTDate();
 	if (validUrl.isUri(originalUrl)) {
-		const queryResult = await ShortenUrl.findOne({ originalUrl: originalUrl }, error => {
-			if (error) return res.status(401).send(`DB Error: ${error}`);
+		const queryResult = await ShortenUrl.findOne({ originalUrl: originalUrl }, err => {
+			if (err) return res.status(401).send(`DB Error: ${err}`);
 		});
 		if (queryResult) {
 			const shortUrl = shortBaseUrl + queryResult.urlCode;
-			const updateResult = await ShortenUrl.findOneAndUpdate(
-				{ originalUrl: originalUrl },
-				{ $set: { shortUrl: shortUrl, updatedAt: updatedAt }},
-				{ new: true },
-				error => {
-				if (error) return res.status(401).send(`DB Error: ${error}`);
+			const updateResult = await ShortenUrl.findOneAndUpdate({ originalUrl: originalUrl }, {$set: { shortUrl: shortUrl, updatedAt: updatedAt }}, { new: true }, err => {
+				if (err) return res.status(401).send(`DB Error: ${err}`);
 			});
 			return updateResult;
 		} else {
@@ -30,8 +26,8 @@ async function createShortenUrl(req, res, originalUrl) {
 				shortUrl,
 				urlCode
 			});
-			await queryResult.save((error) => {
-				if (error) return res.status(401).send(`DB Error: ${error}`);
+			await queryResult.save((err) => {
+				if (err) return res.status(401).send(`DB Error: ${err}`);
 			});
 			return queryResult;
 		}
@@ -48,22 +44,42 @@ route.get('/:urlCode?', async (req, res) => {
 		case 'error':
 			return res.sendFile(path.join(__dirname, '../', 'views/error.html'));
 		default:
-			const queryResult = await ShortenUrl.findOne({ urlCode: urlCode }, error => {
-				if (error) return res.status(401).send(`DB Error: ${error}`);
+			const queryResult = await ShortenUrl.findOne({ urlCode: urlCode }, err => {
+				if (err) return res.status(401).send(`DB Error: ${err}`);
 			});
 			if (queryResult) {
 				const count = queryResult.count + 1;
-				const updateResult = await ShortenUrl.findOneAndUpdate(
-					{ urlCode: urlCode },
-					{ $set: { count: count }},
-					{ new: true },
-					error => {
-					if (error) return res.status(401).send(`DB Error: ${error}`);
+				const updateResult = await ShortenUrl.findOneAndUpdate({ urlCode: urlCode }, {$set: { count: count }}, { new: true }, err => {
+					if (err) return res.status(401).send(`DB Error: ${err}`);
 				});
 				return res.redirect(updateResult.originalUrl);
 			} else {
 				return res.redirect('/error');
 			}
+	}
+});
+
+route.get('/api/find/:urlCode?', async (req, res) => {
+	const urlCode = req.params.urlCode;
+	switch (urlCode) {
+		case undefined:
+			ShortenUrl.find({}, (err, docs) => {
+				if (err) return res.status(401).send(`DB Error: ${err}`);
+				return res.status(200).json(docs);
+			});
+			break;
+		case 'count':
+			ShortenUrl.find({}, null, {sort: {count: -1}}, (err, docs) => {
+				if (err) return res.status(401).send(`DB Error: ${err}`);
+				return res.status(200).json(docs);
+			});
+			break;
+		default:
+			ShortenUrl.findOne({ urlCode: urlCode }, (err, docs) => {
+				if (err) return res.status(401).send(`DB Error: ${err}`);
+				return res.status(200).json(docs);
+			});
+			break;
 	}
 });
 
@@ -92,26 +108,10 @@ route.post('/api/create', async (req, res) => {
 });
 
 route.post('/api/remove', async (req, res) => {
-	ShortenUrl.remove({}, error => {
-		if (error) return res.status(401).send(`DB Error: ${error}`);
+	ShortenUrl.remove({}, err => {
+		if (err) return res.status(401).send(`DB Error: ${err}`);
 		return res.status(200).send('collection removed');
 	});
-});
-
-route.get('/api/find/:urlCode?', async (req, res) => {
-	const urlCode = req.params.urlCode;
-	switch (urlCode) {
-		case undefined:
-			const findResult = await ShortenUrl.find({}, error => {
-				if (error) return res.status(401).send(`DB Error: ${error}`);
-			});
-			return res.status(200).json(findResult);
-		default:
-			const findOneResult = await ShortenUrl.findOne({ urlCode: urlCode }, error => {
-				if (error) return res.status(401).send(`DB Error: ${error}`);
-			});
-			return res.status(200).json(findOneResult);
-	}
 });
 
 module.exports = route;
